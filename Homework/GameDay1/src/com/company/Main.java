b465899ab757bcd44f99ec63f53bd57b4220e4dc
nhatn <nhatnam1711999>
2018-05-15 08:37:28 +0700

    Add: when player moves out, player will appears in the opposite postion.

--- Homework/GameDay1/src/com/company/Main.java
+++ Homework/GameDay1/src/com/company/Main.java
@@ -15,27 +15,27 @@ public class Main {
             System.out.println();
         }
         while (true) {
-            String a = scanner.next();
+            String  a = scanner.next();
             switch (a) {
                 case "a":
                     if (yP > 0) yP--;
-                    else System.out.println("Can't move");
+                    else yP = 3;
                     break;
                 case "s":
                     if (xP < 3) xP++;
-                    else System.out.println("Can't move");
+                    else xP = 0;
                     break;
                 case "d":
                     if (yP < 3) yP++;
-                    else System.out.println("Can't move");
+                    else yP = 0;
                     break;
                 case "w":
                     if (xP > 0) xP--;
-                    else System.out.println("Can't move");
+                    else xP = 3;
                     break;
                 case "x":
                     return;
-            }
+            };
             for (int i = 0; i < 4; i++) {
                 for (int j = 0; j < 4; j++) {
                     if ((i == xP) && (j == yP)) System.out.print('P');
